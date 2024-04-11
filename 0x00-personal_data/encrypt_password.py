5f494548b5746075624519f38a17bab8a237c348
Natnael-Gedlu <nategd800@gmail.com>
2024-04-11 22:45:47 +0300

    Validate a password against its hashed version

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,15 @@ def hash_password(password: str) -> bytes:
         bytes: The hashed password.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate a password against its hashed version.
+    Args:
+        hashed_password (bytes): The hashed password to compare against.
+        password (str): The password to validate.
+    Returns:
+        bool: True if the password is valid, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
