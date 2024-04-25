c033b93387067732097ac4fd0403b7736c87e4a8
Natnael-Gedlu <nategd800@gmail.com>
2024-04-25 14:50:54 +0300

    Define a SQLAlchemy User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Define a SQLAlchemy User model.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    Represents a user with attributes such as email, hashed password,
+    session ID, and reset token.
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
